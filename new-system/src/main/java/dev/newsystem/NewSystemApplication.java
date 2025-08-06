df232120beed81d0992bd6fed67add39257dc582
luustik <kirill.lustoff@gmail.com>
2025-08-06 12:39:50 +0300

    create empty spring boot project new-system

---
+++ new-system/src/main/java/dev/newsystem/NewSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.newsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSystemApplication.class, args);
+    }
+
+}
---
+++ new-system/src/test/java/dev/newsystem/NewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.newsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
